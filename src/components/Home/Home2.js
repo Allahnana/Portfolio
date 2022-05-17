a58f1320b4125af6075170774451af5af11f2043
Allahnana <Usmanallahnana@gmail.com>
2022-05-17 23:32:41 +0100

    Updated my IG profile

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/dev_allahnana/"
+                  href="https://www.instagram.com/dev_abdul/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
